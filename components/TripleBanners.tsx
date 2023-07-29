6443de80c61de0326bb13c666beaa8089ac1c62f
Selim Enes Erdoğan <selim@Selim-M2-Macbook.local>
2023-07-29 16:48:39 +0300

    banner info

--- components/TripleBanners.tsx
+++ components/TripleBanners.tsx
@@ -5,9 +5,15 @@ type Props = {};
 const TripleBanners = (props: Props) => {
   return (
     <section className="my-10 px-10 w-full flex items-center justify-between gap-6">
-      <div className="flex-1 aspect-video bg-gray-200"></div>
-      <div className="flex-1 aspect-video bg-gray-200"></div>
-      <div className="flex-1 aspect-video bg-gray-200"></div>
+      <div className="flex-1 aspect-video bg-gray-200 flex items-center justify-center">
+        Banner 16/9
+      </div>
+      <div className="flex-1 aspect-video bg-gray-200 flex items-center justify-center">
+        Banner 16/9
+      </div>
+      <div className="flex-1 aspect-video bg-gray-200 flex items-center justify-center">
+        Banner 16/9
+      </div>
     </section>
   );
 };
