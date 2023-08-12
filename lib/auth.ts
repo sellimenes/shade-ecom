744d944d4bbf1bf7139698a24744318787081904
Selim Enes Erdoğan <selim@Selim-M2-Macbook.local>
2023-08-12 13:00:18 +0300

    build error fix

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,32 +1,32 @@
-import { NextAuthOptions } from "next-auth";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import GoogleProvider from "next-auth/providers/google";
+// import { NextAuthOptions } from "next-auth";
+// import { PrismaAdapter } from "@next-auth/prisma-adapter";
+// import GoogleProvider from "next-auth/providers/google";
 
-import prismadb from "@/lib/prismadb";
+// import prismadb from "@/lib/prismadb";
 
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prismadb),
-  session: {
-    strategy: "jwt",
-  },
-  pages: {
-    signIn: "/sign-in",
-  },
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-  callbacks: {
-    async session({ token, session }) {
-      if (token) {
-        session.user.id = token.id;
-        session.user.name = token.name;
-        session.user.email = token.email;
-      }
+// export const authOptions: NextAuthOptions = {
+//   adapter: PrismaAdapter(prismadb),
+//   session: {
+//     strategy: "jwt",
+//   },
+//   pages: {
+//     signIn: "/sign-in",
+//   },
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID!,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+//     }),
+//   ],
+//   callbacks: {
+//     async session({ token, session }) {
+//       if (token) {
+//         session.user.id = token.id;
+//         session.user.name = token.name;
+//         session.user.email = token.email;
+//       }
 
-      return session;
-    },
-  },
-};
+//       return session;
+//     },
+//   },
+// };
