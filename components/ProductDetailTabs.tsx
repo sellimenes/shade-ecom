e896cf1c517f4ba36e7b6f16bcc809b10810eeaa
Selim Enes Erdoğan <selim@Selim-M2-Macbook.local>
2023-08-21 12:24:01 +0300

    tabs change color

--- components/ProductDetailTabs.tsx
+++ components/ProductDetailTabs.tsx
@@ -8,10 +8,18 @@ const ProductDetailTabs = (props: Props) => {
   return (
     <Tabs defaultValue="description" className="w-full">
       <TabsList className="w-full justify-start bg-primary text-secondary">
-        <TabsTrigger value="description">Product Description</TabsTrigger>
-        <TabsTrigger value="reviews">Reviews</TabsTrigger>
-        <TabsTrigger value="tags">Tags</TabsTrigger>
-        <TabsTrigger value="additionals">Additional Information</TabsTrigger>
+        <TabsTrigger value="description" className="rounded-r-none">
+          Product Description
+        </TabsTrigger>
+        <TabsTrigger value="reviews" className="rounded-none">
+          Reviews
+        </TabsTrigger>
+        <TabsTrigger value="tags" className="rounded-none">
+          Tags
+        </TabsTrigger>
+        <TabsTrigger value="additionals" className="rounded-none">
+          Additional Information
+        </TabsTrigger>
       </TabsList>
       <TabsContent value="description">
         Product description will be here. All HTML etc. supported.
